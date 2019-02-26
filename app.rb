728f3d30d3bb5d51011310f27bfbe4ed117bad93
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-26 21:58:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,6 +27,10 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
-    
+        @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    erb :operation
+  end
 
 end
